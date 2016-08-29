a2f3911b706e62858c2e39690795b510d6c6d487
James Surgenor <jsurgenor@gmail.com>
2016-08-29 21:52:47 +0100

    added main program logic

--- refman.py
+++ refman.py
@@ -1,22 +1,69 @@
 #!/usr/bin/python
 
 '''
-This is a temporary main file to test the pybibtex lib
+RefMan - an interactive commandline tool for managing and querying simple BibTeX databases
 
-It will eventually contain all the logic for RefMan
+Copyright (c) James Surgenor 2016
 '''
 
 import pybibtex as BibTeX
 
-def main():
+def addMode(document):
+    leave = False
+    while not leave:
+        entryType = ""
+        while entryType == "":
+            entryType = raw_input("\nADD MODE\nEntry type ('q' to quit):\n> ")
+        if entryType == 'q':
+            leave = True
+        else:
+            document.addNewEntry(entryType)
 
+def findMode(document):
+    leave = False
+    while not leave:
+        findType = ""
+        while findType == "":
+            findType = raw_input("\nFIND MODE\nFind type ('q' to quit):\n> ")
+        if findType == 'q':
+            leave = True
+        elif findType == 'h':
+            print("Find Mode - available Find types:")
+            print("a: author, returns all entries with a given author")
+            print("t: title, returns all entries whose title contains given string")
+        elif findType == 'a':
+            author = raw_input("Author:\n> ")
+            finds = [x for x in document.entries if author in x.getField("author")]
+            for find in finds:
+                print("\n" + find.toBibTeXString())
+        else:
+            print("Unknown - exiting safely")
+            leave = True
+
+
+def main():
     doc = BibTeX.BibTeX()
 
-    doc.parse("test.bib")
+    print("RefMan - an interactive commandline BibTeX management tool")
 
-    doc.addNewEntry("booklet")
+    leave = False
+    while not leave:
+        mode = ""
+        while mode == "":
+            mode = raw_input("\nMODE SELECT\nMode ('q' to quit):\n> ")
+        if mode == 'q':
+            leave = True
+        elif mode == 'add':
+            addMode(doc)
+        elif mode == 'find':
+            findMode(doc)
+        else:
+            print("Unknown - exiting safely")
+            leave = True
 
-    doc.write("test.bib")
+    save = raw_input("Save changes? (y/n):\n> ")
+    if save == 'y':
+        doc.write("tmp.bib")
 
 if __name__ == '__main__':
     main()
